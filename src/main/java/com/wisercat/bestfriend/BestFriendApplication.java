405b494ef58daa0fe35282b151e54401249c667b
Danila Romanov <danila.romanof@gmail.com>
2022-12-19 14:17:46 +0200

    Initial state

---
+++ src/main/java/com/wisercat/bestfriend/BestFriendApplication.java
@@ -0,0 +1,13 @@
+package com.wisercat.bestfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisercat/bestfriend/BestFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisercat.bestfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
