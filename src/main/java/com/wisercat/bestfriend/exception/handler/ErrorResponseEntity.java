66b2c9f28c4109303e1c1eb974f1de31a2def63f
Danila Romanov <danila.romanof@gmail.com>
2022-12-22 14:30:15 +0200

    @JsonProperty for ErrorResponseEntity

--- src/main/java/com/wisercat/bestfriend/exception/handler/ErrorResponseEntity.java
+++ src/main/java/com/wisercat/bestfriend/exception/handler/ErrorResponseEntity.java
@@ -1,6 +1,7 @@
 package com.wisercat.bestfriend.exception.handler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ErrorResponseEntity {
+    @JsonProperty(namespace = "name")
     private String name;
+
+    @JsonProperty(namespace = "message")
     private String message;
 }
