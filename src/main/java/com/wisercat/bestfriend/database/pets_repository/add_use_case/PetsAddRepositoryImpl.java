b88442cda44a9d18b414e604ada8cea69750a270
Danila Romanov <danila.romanof@gmail.com>
2022-12-22 16:06:39 +0200

    Repository mock functionality get and add

--- src/main/java/com/wisercat/bestfriend/database/pets_repository/add_use_case/PetsAddRepositoryImpl.java
+++ src/main/java/com/wisercat/bestfriend/database/pets_repository/add_use_case/PetsAddRepositoryImpl.java
@@ -1,11 +1,20 @@
 package com.wisercat.bestfriend.database.pets_repository.add_use_case;
 
 import com.wisercat.bestfriend.dto.PetDto;
+import com.wisercat.bestfriend.exception.DataAlreadyExistsException;
 import com.wisercat.bestfriend.service.pets_service.add_use_case.PetsAddRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PetsAddRepositoryImpl implements PetsAddRepository {
+    private long nextId;
     @Override
     public PetDto save(PetDto petDto) {
-        return null;
+        if (petDto.getId() == null) {
+            petDto.setId(nextId++);
+            return petDto;
+        } else {
+            throw new DataAlreadyExistsException("This data is already exist in database");
+        }
     }
 }
--- src/main/java/com/wisercat/bestfriend/database/pets_repository/get_use_case/PetsGetRepositoryImpl.java
+++ src/main/java/com/wisercat/bestfriend/database/pets_repository/get_use_case/PetsGetRepositoryImpl.java
@@ -1,20 +1,60 @@
 package com.wisercat.bestfriend.database.pets_repository.get_use_case;
 
 import com.wisercat.bestfriend.dto.PetDto;
+import com.wisercat.bestfriend.dto.enums.CountryOrigin;
+import com.wisercat.bestfriend.dto.enums.FurColor;
+import com.wisercat.bestfriend.dto.enums.PetType;
 import com.wisercat.bestfriend.service.pets_service.get_use_case.PetsGetRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class PetsGetRepositoryImpl implements PetsGetRepository {
 
     @Override
     public Optional<PetDto> getById(Long id) {
-        return Optional.empty();
+        return switch (id.intValue()) {
+            case 0 -> Optional.of(new PetDto(
+                    0L,
+                    "10723d04-b391-4ffd-914c-10b8568ada41",
+                    "l2Fb",
+                    PetType.CAT,
+                    FurColor.BLACK,
+                    CountryOrigin.ESTONIA
+            ));
+            case 1 -> Optional.of(new PetDto(
+                    1L,
+                    "b3b5dc30-b915-417a-972c-23499ea1282e",
+                    "e5017htR",
+                    PetType.RABBIT,
+                    FurColor.BROWN,
+                    CountryOrigin.FINLAND
+            ));
+            default -> Optional.empty();
+        };
     }
 
     @Override
     public List<PetDto> getAll() {
-        return null;
+        return List.of(
+                new PetDto(
+                        0L,
+                        "10723d04-b391-4ffd-914c-10b8568ada41",
+                        "l2Fb",
+                        PetType.CAT,
+                        FurColor.BLACK,
+                        CountryOrigin.ESTONIA
+                ),
+                new PetDto(
+                        1L,
+                        "b3b5dc30-b915-417a-972c-23499ea1282e",
+                        "e5017htR",
+                        PetType.RABBIT,
+                        FurColor.BROWN,
+                        CountryOrigin.FINLAND
+                )
+        );
     }
 }
