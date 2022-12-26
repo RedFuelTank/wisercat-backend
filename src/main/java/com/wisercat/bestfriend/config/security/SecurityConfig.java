74a757cd7916b1f04df66251f591133fcae87a3a
Danila Romanov <danila.romanof@gmail.com>
2022-12-26 14:21:58 +0200

    Change in-memory user database to remote database

--- src/main/java/com/wisercat/bestfriend/config/security/SecurityConfig.java
+++ src/main/java/com/wisercat/bestfriend/config/security/SecurityConfig.java
@@ -9,20 +9,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -65,24 +64,21 @@ public class SecurityConfig {
 
 
     @Bean
-    public UserDetailsService getUserDetails(AuthenticationManagerBuilder auth) {
-        UserDetails user = User.builder()
-                .username(userConfig.getUserName())
-                .password(getPasswordEncoder().encode(userConfig.getUserPassword()))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username(userConfig.getAdminName())
-                .password(getPasswordEncoder().encode(userConfig.getAdminPassword()))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService getUserDetails(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    public static void main(String[] args) {
+        String userPasswordEncrypted = new BCryptPasswordEncoder(10).encode("user");
+        String adminPasswordEncrypted = new BCryptPasswordEncoder(10).encode("admin");
+        System.out.println("User password: " + userPasswordEncrypted);
+        System.out.println("Admin password: " + adminPasswordEncrypted);
     }
 
 
